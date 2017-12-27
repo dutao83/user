c00c5c3ff07d45b5444eb3b440208d7944b819ff
I336253 <david.du@sap.com>
2017-12-27 21:37:54 +0800

    support Eureka

--- src/main/groovy/com/shanshui/smartcommunity/user/UserApplication.groovy
+++ src/main/groovy/com/shanshui/smartcommunity/user/UserApplication.groovy
@@ -2,8 +2,12 @@ package com.shanshui.smartcommunity.user
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.feign.EnableFeignClients
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 class UserApplication {
 
     static void main(String[] args) {
